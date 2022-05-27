5ac26372c31bcd4bb51c874cc98e6761bc62ea3d
robynforrest <robynforrest@gmail.com>
2022-05-27 10:48:16 -0700

    update file path

--- R/all.R
+++ R/all.R
@@ -52,7 +52,7 @@ source(file.path(rootd.R, "tables-decisions.R"))
 source(file.path(rootd.R, "plot-survey-sets.R"))
 source(file.path(rootd.R, "get-alk.R"))
 source(file.path(rootd.R, "get-age-sample.R"))
-source(file.path(rootd.data, "get-data.R"))
+source(file.path(rootd.R, "get-data.R"))
 
 ## # Code to setup forecast model runs
 ## # source("forecast-catch-levels.r")
